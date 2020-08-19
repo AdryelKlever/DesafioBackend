1865f3ad4673d536a717a58fd1d2fbd57a26b148
adryelklever <adryelklever3m@hotmail.com>
2020-08-18 22:41:18 -0300

    Criado diretório Api

---
+++ DesafioBackEnd/Api/Response/EspecialistaResponse.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DesafioBackEnd.Api.Response
+{
+    public class EspecialistaResponse
+    {
+        public int IdEspecilidade { get; set; }
+        public string NomeEspecialidade { get; set; }
+    }
+}
